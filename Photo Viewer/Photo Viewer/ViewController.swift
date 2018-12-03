1f9fdb4fc213db400f7c0d5536bb9cd675e0b756
Tristan Darwent <tristandarwent@gmail.com>
2018-12-03 14:16:24 -0500

    Initial xcode commit

---
+++ Photo Viewer/Photo Viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Viewer
+//
+//  Created by Tristan Darwent on 2018-12-03.
+//  Copyright © 2018 Tristan Darwent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
